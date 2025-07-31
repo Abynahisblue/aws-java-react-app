1e21beb5448b8d7ca6e4dee9050b48d1db4bacc0
Abynahisblue <sandra.kumi@amalitech.com>
2025-07-31 14:12:10 +0000

    fix: app health issues

--- webapp/src/main/java/com/webapp/WebappApplication.java
+++ webapp/src/main/java/com/webapp/WebappApplication.java
@@ -2,12 +2,15 @@ package com.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class WebappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WebappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WebappApplication.class, args);
+    }
 }
